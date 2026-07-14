3cdb2813d70018dcbd44a211a7bf75ef8cb808f7
AbdelellahHazem <141696492+AbdelellahHazem@users.noreply.github.com>
2026-07-14 22:20:48 +0300

    create home rep imp

---
+++ lib/features/home/data/repos/home_repo_imp.dart
@@ -0,0 +1,45 @@
+import 'package:clean_arch_bookly_app/core/errors/failure.dart';
+import 'package:clean_arch_bookly_app/features/home/data/data_sources/home_local_data_source.dart';
+import 'package:clean_arch_bookly_app/features/home/data/data_sources/home_remote_data_source.dart';
+import 'package:clean_arch_bookly_app/features/home/domain/entities/home_entity.dart';
+import 'package:clean_arch_bookly_app/features/home/domain/repos/home_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class HomeRepoImp extends HomeRepo {
+  final HomeRemoteDataSource homeRemoteDataSource;
+  final HomeLocalDataSource homeLocalDataSource;
+  HomeRepoImp({
+    required this.homeRemoteDataSource,
+    required this.homeLocalDataSource,
+  });
+
+  @override
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() async {
+    try {
+      List<BookEntity> books;
+      books = await homeLocalDataSource.fetchFeaturedBooks();
+      if (books.isNotEmpty) {
+        return Right(books);
+      }
+      books = await homeRemoteDataSource.fetchFeaturedBooks();
+      return Right(books);
+    } catch (e) {
+      return Left(Failure());
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<BookEntity>>> fetchNewestBooks() async {
+    try {
+      List<BookEntity> books;
+      books = await homeLocalDataSource.fetchNewestBooks();
+      if (books.isNotEmpty) {
+        return Right(books);
+      }
+      books = await homeRemoteDataSource.fetchNewestBooks();
+      return Right(books);
+    } catch (e) {
+      return Left(Failure());
+    }
+  }
+}
